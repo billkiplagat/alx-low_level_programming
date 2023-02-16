def0d623ce3fe65228e06d5175011ca2ddf6b11c
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-16 08:21:32 +0000

    Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - This is my third main function
+*Description: The program outputs the size of different datatypes in 32 and 64 bit machines
+*Return: 0 for now
+*/
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+return (0);
+}
