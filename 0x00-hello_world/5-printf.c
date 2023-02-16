92a877d179315a8b5398b785baaedd6737315c54
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-16 07:50:18 +0000

    printf update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - This is my second main function
+*Description: The program uses printf function to output a string
+*Return: 0 for now
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
