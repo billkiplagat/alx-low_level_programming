5cbbfd73fb1fd3e9705e0d5a34eec9cce35c9813
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-16 07:37:23 +0000

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-*main-This is my first main function
+*main - This is my first main function
+*Description: The program uses puts function to output a string
+*Return: 0 for now
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/**
-*Return: 0 for now
-*/
 return (0);
 }
