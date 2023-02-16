af1a769b7fb56a4c1b50623ef10a1af90d6b543c
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-16 10:45:43 +0000

    quote update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 *main - Entry point
-*Description: Outputs using stderr
+*Description: Outputs using write function
 *Return: 1
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-printf("\n");
+write(STDOUT_FILENO, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
