4a38c0a685652a3115154bccb67c0da8b41e6367
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-16 09:25:38 +0000

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - This is the entry point
+*Description: The program outputs a string using the stderr
+*Return: 1
+*/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
