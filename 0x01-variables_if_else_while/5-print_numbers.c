4f2ffc6359de99d23c7aed6a42ae2a12b9b20af9
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-17 11:12:13 +0000

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - this is the exit point
+*Description: the program use while loop to print numbers
+*Return: is 0
+*/
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+printf("\n");
+return (0);
+}
