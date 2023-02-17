a5122888f431fbde88b56b1ce237d2ad1a7afbe4
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-17 10:56:09 +0000

    updated print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 /**
 *main - this is the exit point
-*Description: the program use for loop to print a-z
+*Description: the program use while loop to print a-z
 *Return: is 0
 */
 int main(void)
 {
-for (int i = 'a'; i <= 'z' ; i++)
-	putchar(i);
+int i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
 putchar('\n');
 return (0);
 }
