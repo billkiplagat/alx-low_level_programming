66ad99e41bb41457fe1beecb5e16684c035934fc
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-17 13:02:16 +0000

    printing combo3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - this is the exit point
+*Description: the program use while loop to print 2 digit combination
+*Return: is 0
+*/
+int main(void)
+{
+int i = 0, j;
+while (i < 9)
+{
+j = 1;
+while (j <= 9)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i != 8 || j != 9)
+{
+putchar(44);
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
