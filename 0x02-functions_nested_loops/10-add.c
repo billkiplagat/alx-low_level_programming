aab35e9f04805507aded6c56abd50a5eea121144
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-21 19:56:30 +0300

    updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add -  function
+*@a: used to get 1st number
+*@b: used to get second number
+*Description: used to add 2 numbers
+*Return: 0 for now
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
