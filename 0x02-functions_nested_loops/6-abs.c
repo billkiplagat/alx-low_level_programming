f361245ef9f96f27c120e4aaf63b8f23c0689f37
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-21 17:51:37 +0300

    absolute values only

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs -  function
+ * @x: used to get inputs
+ * Description: used to print absolute value of an integer.
+ * Return: 0 for now
+ */
+int _abs(int x)
+{
+if (x < 0)
+{
+return (-x);
+}
+else
+{
+return (x);
+}
+}
