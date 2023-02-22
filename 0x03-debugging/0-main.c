71a550e401ddc33bf5d54de09ddd3889a546a5cc
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-22 18:46:34 +0300

    updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
  * main - execution point
  * Description: to test a function
  * Return: 0 for now
 */
+
 int main(void)
 {
-int i = 0;
+int i;
+
+i = 0;
 positive_or_negative(i);
 return (0);
 }
