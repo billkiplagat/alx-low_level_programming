cb34d9fdc661c61f58056b853ea2743e90b0c5f5
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-23 22:24:25 +0300

    updated fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - main execution point
+ * Description: the code prints 1 to 100 but with some modification
+ * Return: 0 for now
+ */
+int main(void)
+{
+int i = 1;
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+printf("%d ", i);
+i++;
+}
+printf("\n");
+return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _isupper(int);
 int _isdigit(int);
