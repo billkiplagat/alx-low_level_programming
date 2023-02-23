c25bbbc0d2dd33d1503ca6f4c5fff4c45106ec6d
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-23 22:48:22 +0300

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,19 @@ int main(void)
 int i = 1;
 while (i <= 100)
 {
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
-}
-else if (i % 3 == 0 && i % 5 != 0)
-{
+if (i % 3 == 0 && i % 5 != 0)
 printf(" Fizz");
-}
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (i == 1)
+{
+printf("%d", i);
 }
 else
 printf(" %d", i);
