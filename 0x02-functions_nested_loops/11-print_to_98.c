3b5d996457791c41a63a063990f4887f9743f458
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-25 22:25:20 +0300

    updated

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+/**
+ * print_to_98 - function to print numbers to 98
+ * @n: used to get input
+ * Description: printing to 98
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,5 +19,6 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 void times_table(void);
+void print_to_98(int);
 int add(int, int);
 #endif
