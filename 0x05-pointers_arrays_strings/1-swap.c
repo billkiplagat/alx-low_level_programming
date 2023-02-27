dee52f39d37cb2beb0ce89fa69218c90e36b25ca
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-27 18:02:53 +0300

    updated number 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - function to swap numbers
+ * @a: get first number
+ * @b: get second number
+ * Description: the function swaps 2 numbers
+ */
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
