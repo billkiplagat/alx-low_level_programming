91a300557e7b49e9bf299dfc926f55658e8bd3fc
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-27 21:51:43 +0300

    updated puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts_half - function to print half chars
+ * @str: to get char inputs
+ * Description: the function prints half of the string
+ */
+void puts_half(char *str)
+{
+int i, len;
+len = strlen(str);
+for (i = len / 2; i <= len - 1; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
