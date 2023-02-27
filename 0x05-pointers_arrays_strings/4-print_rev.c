aa7c2e2bc659ef523073c91913366cac62261ef6
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-27 20:27:32 +0300

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_rev - function to print chars
- * @s to get char inputs
+ * @s: to get char inputs
  * Description: the function prints a string in reverse
  */
 void print_rev(char *s)
 {
-int i,len;
+int i, len;
 len = strlen(s);
 for (i = len - 1; i >= 0; i--)
 {
