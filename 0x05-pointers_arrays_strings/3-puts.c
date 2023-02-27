e5eb1042bebd7d79487ee7ebfe28cef25612caa1
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-27 19:04:48 +0300

    updated _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - function to print characters
+ * @str: used to get char inputs
+ * Description: used to print a string of chars
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0' ; i++)
+{
+printf("%c", str[i]);
+}
+}
