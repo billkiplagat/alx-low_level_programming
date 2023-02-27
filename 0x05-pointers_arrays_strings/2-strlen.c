8cd902b39928ae0f40a28befa6860cd67e52f347
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-27 18:48:57 +0300

    updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function
+ * @s: to get char input
+ * Description: prints the number of char in a string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int length = 0;
+for ( ; s[length] != '\0'; length++)
+{
+}
+return (length);
+}
