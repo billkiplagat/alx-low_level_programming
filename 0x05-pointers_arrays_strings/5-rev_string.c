7c19278f18137fb41dce9d17d79cfdc6d05f08f3
billkiplagat <bill.kiplagat.7@gmail.com>
2023-02-27 21:02:44 +0300

    updated

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string: function prints strings
+ * @s: used to get inputs
+ * Description: the function is used to reverse a string
+ */
+void rev_string(char *s)
+{
+int i;
+char temp;
+int len = strlen(s);
+for (i = 0; i < len/2 ; i++)
+{
+temp = s[i];
+s[i] = s[len-1-i];
+s[len - 1 - i] = temp;
+}
+}
