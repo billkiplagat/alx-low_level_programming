b61d8ba2429bf9f0784712bb7d5c7cb6d997b85a
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-01 20:04:56 +0300

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,8 +15,11 @@ if (s1[i] != s2[i])
 {
 return (s1[i] - s2[i]);
 }
-i++;
-return (s1[i] - s1[i]);
+if (s1[i] == '\0' && s2[i] == '\0')
+{
+return (0);
 }
-return (s2[i] - s1[i]);
+i++;
+}
+return (s1[i] - s1[i]);
 }
