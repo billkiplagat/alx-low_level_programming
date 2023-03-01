1d3b9fbe1c3f90e559d8e7750129714eb23f8262
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-01 19:41:56 +0300

    updated

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - function
+ * @s1: used to get string 1
+ * @s2: used to get string 2
+ * Description: function to compare 2 strings
+ * Return: multiple returns
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+else
+{
+return (s1[i] - s1[i]);
+}
+i++;
+}
+return (s2[i] - s1[i]);
+}
