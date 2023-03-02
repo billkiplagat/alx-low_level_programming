41ab9051988c438736ce06fa649a90f827ad92d1
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-02 06:35:29 +0300

    updated

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - function to capitalize string
+ * @p: to get string inputs
+ * Description: the function capitalize first char in a word
+ * Return: p
+ */
+char *cap_string(char *p)
+{
+int i, word = 1;
+for (i = 0; p[i] != '\0'; i++)
+{
+if (word && p[i] >= 'a' && p[i] <= 'z')
+{
+p[i] = p[i] - 'a' + 'A';
+}
+if ((p[i] == ' ' || p[i] == '\t' || p[i] == '\n' ||
+p[i] == ',' || p[i] == ';' || p[i] == '.' ||
+p[i] == '!' || p[i] == '?' || p[i] == '"' ||
+p[i] == '(' || p[i] == ')' || p[i] == '-')
+|| p[i] == '{' || p[i] == '}')
+{
+word = 1;
+}
+else
+{
+word = 0;
+}
+}
+return (p);
+}
