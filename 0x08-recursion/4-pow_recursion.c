69e930a9ae5887ad849b20858ff2529e052ed2ff
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-08 17:57:02 +0300

    fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,6 +8,10 @@
  */
 int _pow_recursion(int x, int y)
 {
+if (y < 0)
+{
+return (-1);
+}
 if (y == 0)
 {
 return (1);
@@ -15,9 +19,5 @@ return (1);
 else if (y > 0)
 {
 return (x * _pow_recursion(x, y - 1));
-}
-else
-{
-return (1 / _pow_recursion(x, -y));
 }
 }
