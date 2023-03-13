9716222759e6c17fcf23bec0d4d54f4744855461
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-13 12:13:42 +0300

    updated condition

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,14 @@ char *str_concat(char *s1, char *s2)
 {
 size_t s1_len, s2_len;
 char *new_mem;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 s1_len = strlen(s1);
 s2_len = strlen(s2);
 new_mem = malloc((s1_len + s2_len) +1);
