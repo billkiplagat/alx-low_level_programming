37937ecd0e834b203240ded4c95b3a57a984f0d0
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-13 14:23:02 +0300

    updated return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@
 int **alloc_grid(int width, int height)
 {
 int **new_mem;
-int i;
+int i, j;
 if (width <= 0 || height <= 0)
 {
 return (NULL);
@@ -18,10 +18,15 @@ new_mem = (int **) malloc(height * sizeof(int *));
 for (i = 0; i < height; i++)
 {
 new_mem[i] = (int *) malloc(width * sizeof(int));
-}
-if (*new_mem == NULL)
+if (new_mem[i] == NULL)
 {
+for (j = 0; j < i; j++)
+{
+free(new_mem[j]);
+}
+free(new_mem);
 return (NULL);
+}
 }
 memset(new_mem[0], 0, height * width * sizeof(int));
 return (new_mem);
