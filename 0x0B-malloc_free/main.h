dbd7641deee7fbf7ca3d6f40dd765823677d3320
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-13 09:06:52 +0300

    updated main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
-#define NULL 0
+#define NULL ((void *)0)
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
