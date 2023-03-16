9fa25aa8dba58a06850067dee7368084faba6324
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-16 12:26:46 +0300

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,27 +1,36 @@
+#include "main.h"
+/**
+ * string_nconcat - function to concatenate 2 strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: number of bits to copy
+ * Description: the function copies s2 to s1 n number of times
+ * Return: p pointer to new memory
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int i;
-    int len1, len2;
-    char *p;
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    len1 = strlen(s1);
-    len2 = strlen(s2);
-    if (n >= len2)
-        n = len2;
-    p = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
-    if (p == NULL)
-        return (0);
-    for (i = 0; i < len1; i++)
-    {
-       p[i] = s1[i];
-    }
-    for (; i < (len1 + n); i++)
-    {
-        p[i] = s2[i - len1];
-    }
-    p[i] = '\0';
-    return p;
+unsigned int i;
+unsigned int len1, len2;
+char *p;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+p = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+if (p == NULL)
+return (0);
+for (i = 0; i < len1; i++)
+{
+p[i] = s1[i];
+}
+for (; i < (len1 + n); i++)
+{
+p[i] = s2[i - len1];
+}
+p[i] = '\0';
+return (p);
 }
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
-C language repository
-Debugging projects in c
+More malloc, realloc, calloc, exit and free repository
+TASKS
+#0 - 0-malloc_checked.c - The function is used to allocate memory that can be used to store different type of datatypes so void is used when declaring the pointer
