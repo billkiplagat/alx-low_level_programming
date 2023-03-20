037cc29e43f1addfe895075f86cb7c6a57757dfa
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-20 12:54:15 +0300

    updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <libgen.h>
+/**
+ * main - execution point
+ * Ddescription: function to print file name
+ * Return: 0
+ */
 int main(void)
 {
-    printf("%s\n",basename(__FILE__));
-    return (0);
+printf("%s\n", basename(__FILE__));
+return (0);
 }
