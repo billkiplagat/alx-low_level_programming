842a2d568102369c34328f6ca9dd0d37b8ccab1f
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-20 19:09:36 +0300

    updated n and conditional statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,11 +8,11 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return;
 if ((*d).name == NULL)
-printf("Name: (nil)");
+printf("Name: (nil)\n");
 if ((*d).owner == NULL)
-printf("Owner: (nil)");
-if ((*d).age == 0)
-printf("Age: (nil)");
+printf("Owner: (nil)\n");
+if ((*d).age < 0)
+printf("Age: (nil)\n");
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
