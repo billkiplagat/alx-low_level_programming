d0659f31253cfa7670a62da7c20bbe3a4d2a7073
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-20 18:57:15 +0300

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,9 +8,8 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return;
 if ((*d).name == NULL)
-(*d).name = "(nil)";
+printf("Name: (nil)");
 if ((*d).owner == NULL)
-(*d).owner = "(nil)";
 printf("Owner: (nil)");
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
