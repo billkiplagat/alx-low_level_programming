d5c59ec223942cfcc7c99dd84ae7f402591cce5a
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-21 11:10:50 +0300

    updated condition statement in print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,9 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-if ((*d).name == NULL)
-printf("Name: (nil)\n");
-if ((*d).owner == NULL)
-printf("Owner: (nil)\n");
-if ((*d).age < 0)
-printf("Age: (nil)\n");
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
