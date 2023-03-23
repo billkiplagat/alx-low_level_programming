8963d5d58a389bcbfdc4fdb402887215ccc3b55b
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-23 21:08:12 +0300

    printing all function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,51 @@
 #include "variadic_functions.h"
 /**
+* print_char - print all chars in CL
+* @args: list of arguments
+*
+*/
+void print_char(va_list args)
+{
+char c;
+c = va_arg(args, int);
+printf("%c", c);
+}
+/**
+* print_int - print all integer numbers in CL
+* @args: list of arguments
+*
+*/
+void print_int(va_list args)
+{
+int num;
+num = va_arg(args, int);
+printf("%d", num);
+}
+/**
+* print_float - print all float numbers in CL
+* @args: list of arguments
+*
+*/
+void print_float(va_list args)
+{
+float f;
+f = va_arg(args, double);
+printf("%f", f);
+}
+/**
+* print_string - print all strings in CL
+* @args: list of arguments
+*
+*/
+void print_string(va_list args)
+{
+char *str;
+str = va_arg(args, char *);
+if (str == NULL)
+printf("(nil)");
+printf("%s", str);
+}
+/**
 * print_all - print all strings in CL
 * @format: pointer to char of formats
 *
@@ -7,47 +53,28 @@
 void print_all(const char *const format, ...)
 {
 va_list args;
-int i, num_args = 0;
-char *string;
-double f;
-char c;
-int n;
+int i = 0, j;
+char *separator = "";
+print_t funcs[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string}
+};
 va_start(args, format);
-i = 0;
-while (format[i] != '\0')
+while (format && (*(format + i)))
 {
-switch (format[i])
+j = 0;
+while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+j++;
+if (j < 4)
 {
-case 'c':
-{
-c = va_arg(args, int);
-printf("%c", c);
-break;
+printf("%s", separator);
+funcs[j].print(args);
+separator = ", ";
 }
-case 'i':
-{
-n = va_arg(args, int);
-printf("%d", n);
-break;
-}
-case 'f':
-{
-f = va_arg(args, double);
-printf("%f", f);
-break;
-}
-case 's':
-string = va_arg(args, char *);
-if (string == NULL)
-printf("(nil)");
-printf("%s", string);
-break;
-}
-if (num_args > 0 && format[i + 1] != '\0')
-printf(", ");
-num_args++;
 i++;
 }
-va_end(args);
 printf("\n");
+va_end(args);
 }
