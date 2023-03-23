45ac173cc7ab5fb3df2a76e5c4941bca4fe3f091
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-23 12:14:03 +0300

    updated variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum up all arguments in CL
+ * @n: arguments number
+ *
+ * Return: sum or 0 when n is 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i, sum = 0;
+if (n == 0)
+return (0);
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+va_end(args);
+return (sum);
+}
