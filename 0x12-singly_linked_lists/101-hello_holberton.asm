6e03a063917426c5e0b8b6102264f8c8e0d91b5d
billkiplagat <bill.kiplagat.7@gmail.com>
2023-03-30 20:33:26 +0300

    completed advanced tasks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 10, 0    ; null-terminated string with newline character
-
-section .text
-    global main
-    extern printf
-
+          global    main
+          extern    printf
 main:
-    push rbp                 ; save base pointer
-    mov rbp, rsp             ; set base pointer to stack pointer
-
-    ; call printf function to print hello string
-    mov rdi, hello           ; set first argument (format string)
-    xor eax, eax             ; clear eax register (return value)
-    call printf              ; call printf function
-
-    ; clean up and return
-    mov eax, 0               ; set return value to 0
-    leave                    ; restore stack pointer and base pointer
-    ret                      ; return to operating system
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
