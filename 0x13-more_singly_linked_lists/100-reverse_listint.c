5cfa10587db6d25ac053814cfaa997960af47031
billkiplagat <bill.kiplagat.7@gmail.com>
2023-04-04 08:56:05 +0300

    updated reverse function

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,6 +8,10 @@
 listint_t *reverse_listint(listint_t **head)
 {
 listint_t *current = *head, *temp;
+if (*head == NULL || (*head)->next == NULL)
+{
+return (*head);
+}
 while (current->next != NULL)
 {
 temp = current->next;
@@ -15,5 +19,6 @@ current->next = temp->next;
 temp->next = *head;
 *head = temp;
 }
+current->next = NULL;
 return (*head);
 }
