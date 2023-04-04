74298499e295bbda9b6b52e642908e1010258d70
billkiplagat <bill.kiplagat.7@gmail.com>
2023-04-04 08:03:09 +0300

    updated free function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,14 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *current = *head, *next;
-while (current != NULL)
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head != NULL)
 {
-next = current->next;
-free(current);
-current = next;
+temp = (*head)->next;
+free(*head);
+*head = temp;
 }
 *head = NULL;
 }
