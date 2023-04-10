3e450d9f959c2a293ded00b665dc11a3b492315e
billkiplagat <bill.kiplagat.7@gmail.com>
2023-04-10 14:02:38 +0300

    updated if condition

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define BUF_SIZE 1024
 /**
  * main - execution point
  * @argc: ...
@@ -10,22 +9,22 @@
 int main(int argc, char *argv[])
 {
 int fd_from, fd_to, bytes;
-char buffer[BUF_SIZE];
+char buffer[1024];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 exit(97);
 }
 fd_from = open(argv[1], O_RDONLY);
-if (fd_from < 0 || argv[1] == NULL)
+if (fd_from == -1 || argv[1] == NULL)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-while ((bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
+while ((bytes = read(fd_from, buffer, 1024)) > 0)
 {
-if (fd_to < 0 || bytes != write(fd_to, buffer, bytes))
+if (fd_to == -1 || bytes != write(fd_to, buffer, bytes))
 {
 dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 exit(99);
