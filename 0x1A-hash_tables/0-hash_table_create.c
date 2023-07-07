db776424bb0245aca5fef108793bce9e7e0eb00b
billkiplagat <bill.kiplagat.7@gmail.com>
2023-07-07 10:51:10 +0300

    Updated line

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,17 +14,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 unsigned long int i;
 hash_table_t *table = malloc(sizeof(hash_table_t));
 if (table == NULL)
-return NULL;
+return (NULL);
 
 table->size = size;
 table->array = malloc(size * sizeof(hash_node_t *));
-if (table->array == NULL) {
+if (table->array == NULL)
+{
 free(table);
-return NULL;
+return (NULL);
 }
 
 for (i = 0; i < size; i++)
 table->array[i] = NULL;
 
-return table;
+return (table);
 }
