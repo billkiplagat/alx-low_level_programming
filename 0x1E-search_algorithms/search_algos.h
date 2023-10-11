a7d6f3c6487e96078e178e5d9baaa80e4ac1289e
billkiplagat <bill.kiplagat.7@gmail.com>
2023-10-11 10:58:58 +0300

    Updated mandatory tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,39 +8,33 @@
 
 /* STRUCTS */
 /**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+* struct listint_s - singly linked list
+*
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+*/
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+int n;
+size_t index;
+struct listint_s *next;
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
+* struct skiplist_s - Singly linked list with an express lane
+*
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+* @express: Pointer to the next node in the express lane
+*/
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
 } skiplist_t;
 
 /* PROTOTYPES */
@@ -52,10 +46,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/* UTILITIES */
-listint_t *create_list(int *array, size_t size);
-void free_list(listint_t *list);
-void print_list(const listint_t *list);
 
 #endif
